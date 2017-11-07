09f5c2a2861eea1b599066d1c01b0277a257d614
gurkaran164 <gurkaran_singh164@yahoo.com>
2017-11-07 17:50:35 +0530

    upload

---
+++ README.md
@@ -0,0 +1,1 @@
+#Aquarin Bev
---
+++ app/src/main/java/aquarianbev/aquarianbev/BB.java
@@ -0,0 +1,13 @@
+package aquarianbev.aquarianbev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bb);
+    }
+}
